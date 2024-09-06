ef7b8770835d00e4b1e9b98bba5c38071933d65a
devedzic <devedzic@gmail.com>
2024-09-07 00:03:54 +0200

    Class 1 - main, inception

---
+++ blankscripts/inceptionblank.py
@@ -0,0 +1,39 @@
+"""The very first module in a more structured version of the project.
+"""
+
+
+#%%
+# Moving part of the code from main.py
+
+
+#%%
+# Printing with ' ' and printing without '\n'
+
+
+#%%
+# A simple function and function call
+
+
+#%%
+# Printing docstrings
+
+
+#%%
+# break and continue
+
+
+#%%
+# Importing from Standard Library
+
+# Date format strings
+# https://docs.python.org/3/library/datetime.html?highlight=date%20format%20strings#strftime-and-strptime-format-codes
+
+
+#%%
+# Testing print(__file__)
+
+
+#%%
+# Taking care of the module __name__
+
+
